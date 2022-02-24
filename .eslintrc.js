c3cd22075e2a11b6c2a16eab8ff6753963e867bd
Matheus Abreu <theus.santosab@gmail.com>
2022-02-24 20:41:09 -0300

    inital commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      tsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+import Header from './components/Header';
+import container from './styles/global.module.css';
+
+function App() {
+  return <Header />;
+}
+
+export default App;
